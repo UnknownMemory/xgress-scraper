1ad95a680ce6408e492b59fd6d1eadc1c3064da1
Jérémy <jeremyschneider21@gmail.com>
2023-10-09 21:13:08 +0200

    add database class

---
+++ xgress-scraper.py
@@ -0,0 +1,4 @@
+from xgress_scraper import Database
+
+if __name__ == "__main__":
+    xgressDB: Database = Database()
---
+++ xgress_scraper/db.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import sqlite3
+
+ROOT_DIR: Path = Path(__file__).parent.parent.absolute()
+
+
+class Database:
+
+    def __init__(self) -> None:
+        self.con: sqlite3.Connection = sqlite3.connect(f'{ROOT_DIR}/xgress.db')
+        self.cursor = self.con.cursor()
+        self._init_db()
+
+    def _init_db(self) -> None:
+        print("Database Initialization...\n")
+
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS location (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            address TEXT NOT NULL,
+            description TEXT NOT NULL,
+            lon REAL NOT NULL,
+            lat REAL NOT NULL,
+            UNIQUE(lon, lat) ON CONFLICT IGNORE)
+            ''')
+        self.con.commit()
