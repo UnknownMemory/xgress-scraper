582063d20594dbfb3fce6cf2760ce15964ea3d26
Jérémy <jeremyschneider21@gmail.com>
2023-10-11 02:50:46 +0200

    add insert methods

--- xgress_scraper/db.py
+++ xgress_scraper/db.py
@@ -7,12 +7,19 @@ ROOT_DIR: Path = Path(__file__).parent.parent.absolute()
 class Database:
 
     def __init__(self) -> None:
-        self.con: sqlite3.Connection = sqlite3.connect(f'{ROOT_DIR}/xgress.db')
+        self.con: sqlite3.Connection = sqlite3.connect(f'{ROOT_DIR}/xgress.db', isolation_level=None)
         self.cursor = self.con.cursor()
         self._init_db()
 
     def _init_db(self) -> None:
         print("Database Initialization...\n")
+
+        # optimization
+        self.cursor.execute('PRAGMA journal_mode = OFF;')
+        self.cursor.execute('PRAGMA synchronous = 0;')
+        self.cursor.execute('PRAGMA cache_size = 1000000;')
+        self.cursor.execute('PRAGMA locking_mode = EXCLUSIVE;')
+        self.cursor.execute('PRAGMA temp_store = MEMORY;')
 
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS location (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,3 +34,20 @@ class Database:
             UNIQUE(lon, lat) ON CONFLICT IGNORE)
             ''')
         self.con.commit()
+
+    def begin_transaction(self, batch: list[tuple]) -> str:
+        self.cursor.execute('BEGIN')
+        self.cursor.executemany(
+            'INSERT INTO location (name, pguid, short, img, address, description, lon, lat) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            batch)
+
+        return "Transaction added"
+
+    def end_transaction(self) -> str:
+        try:
+            self.con.commit()
+        except sqlite3.OperationalError:
+            self.con.rollback()
+            return 'Failed to commit transactions\n'
+
+        return "Data successfully saved to the database\n"
--- xgress_scraper/scraper.py
+++ xgress_scraper/scraper.py
@@ -1,21 +1,31 @@
+from xgress_scraper import Database
+
+
 class XgressScraper:
 
     def __init__(self) -> None:
         self.host: str = "https://xgress.com/api/v3"
+        self.database = Database()
 
-    def to_tuple(self, portals: list[dict]) -> list[tuple]:
+    def add_portals(self, portals: list[dict]) -> None:
         portals_tuples = [self._process_portal(portal) for portal in portals]
 
-        return portals_tuples
+        self.database.begin_transaction(portals_tuples)
+        self.database.end_transaction()
 
     @staticmethod
     def _process_portal(portal: dict) -> tuple:
+        previous_key: str = ''
         portal_tuple: tuple = ()
 
         for key, value in portal.items():
+            if previous_key == 'address' and key != 'description':
+                portal_tuple += ('',)
             if key in ['late6', 'lnge6']:
-                portal_tuple = portal_tuple + (value / 1e6,)
-            elif key != 'location':
-                portal_tuple = portal_tuple + (value,)
+                portal_tuple += (value / 1e6,)
+            elif key not in ['location', 'status']:
+                portal_tuple += (value,)
+
+            previous_key = key
 
         return portal_tuple
